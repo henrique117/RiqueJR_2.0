78db605d4a29b3e4f530841f5956040d8a18fcec
Henrique <hassis.moreira177@hotmail.com>
2024-03-25 08:10:16 -0300

    tigrinho safado

--- commands/transfer.js
+++ commands/transfer.js
@@ -17,29 +17,32 @@ module.exports = {
     async execute(interaction) {
         const idToAdd = interaction.options.getUser('id')
         const amountToTransfer = interaction.options.getString('amount')
-        const commandUser = interaction.user
-
-        const rowAdd = await Usuario.findOne({ where: { user_id: idToAdd.id } })
-        const rowRemove = await Usuario.findOne({ where: { user_id: commandUser.id } })
         
-        if(commandUser.id !== idToAdd.id) {
-            if(rowAdd && rowRemove) {
-                const newAmountAdded = rowAdd.balance + parseInt(amountToTransfer)
-                const newAmountRemoved = rowRemove.balance - parseInt(amountToTransfer)
-    
-                if(newAmountRemoved > 0) {
-                    const adding = await Usuario.update({ balance: newAmountAdded }, { where: { user_id: idToAdd.id } })
-                    const removing = await Usuario.update({ balance: newAmountRemoved }, { where: { user_id: commandUser.id } })
-    
-                    interaction.reply(`${amountToTransfer} coins foram tranferidos para *${rowAdd.nome}*`)
+        if(parseInt(amountToTransfer) > 0) {
+            const commandUser = interaction.user
+
+            const rowAdd = await Usuario.findOne({ where: { user_id: idToAdd.id } })
+            const rowRemove = await Usuario.findOne({ where: { user_id: commandUser.id } })
+            
+            if(commandUser.id !== idToAdd.id) {
+                if(rowAdd && rowRemove) {
+                    const newAmountAdded = rowAdd.balance + parseInt(amountToTransfer)
+                    const newAmountRemoved = rowRemove.balance - parseInt(amountToTransfer)
+        
+                    if(newAmountRemoved > 0) {
+                        const adding = await Usuario.update({ balance: newAmountAdded }, { where: { user_id: idToAdd.id } })
+                        const removing = await Usuario.update({ balance: newAmountRemoved }, { where: { user_id: commandUser.id } })
+        
+                        interaction.reply(`${parseInt(amountToTransfer)} coins foram tranferidos para *${rowAdd.nome}*`)
+                    } else {
+                        interaction.reply('Você não tem coins o suficiente pra fazer a transação')
+                    }
                 } else {
-                    interaction.reply('Você não tem coins o suficiente pra fazer a transação')
+                    interaction.reply('Algum desses usuários não está cadastrado')
                 }
             } else {
-                interaction.reply('Algum desses usuários não está cadastrado')
+                interaction.reply('Você não pode transferir coins pra você mesmo!')
             }
-        } else {
-            interaction.reply('Você não pode transferir coins pra você mesmo!')
-        }
+        } else interaction.reply('Insira um valor válido!')
     }
 }
--- index.js
+++ index.js
@@ -79,54 +79,59 @@ client.on('messageCreate', async (message) => {
 })
 
 client.on('messageCreate', async (message) => {
-    if (message.content === prefix + 'tigrinho') {
+    if (message.content.startsWith(prefix + 'tigrinho')) {
         try {
             const randomNum = Math.random() * 100
             const whichValue = message.content.split(' ')
 
             try {
                 const bet = parseInt(whichValue[1])
-                const user = message.author.id
-                const row = await Usuario.findOne({ where: { user_id: user } })
+                if(bet > 0) {
+                    const user = message.author.id
+                    const row = await Usuario.findOne({ where: { user_id: user } })
 
-                if (row.balance - bet >= 0) {
-                    const betting = await Usuario.update({ balance: row.balance - bet }, { where: { user_id: user } })
+                    if (row.balance - bet >= 0) {
+                        const betting = await Usuario.update({ balance: row.balance - bet }, { where: { user_id: user } })
+                        const newRowBalance = await Usuario.findOne({ where: { user_id: user } })
 
-                    if (randomNum < 35) {
-                        message.reply('Vish ce sifudeu e perdeu tudo')
-                    } else if (randomNum < 55 && randomNum >= 35) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 1.1 }, { where: { user_id: user } })
-                        message.reply(`Nice mlk, ganhou **${bet * 1.1} coins**, 1.1x suave`)
-                    } else if (randomNum < 70 && randomNum >= 55) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 1.2 }, { where: { user_id: user } })
-                        message.reply(`Boa demais, ganhou **${bet * 1.2} coins**, 1.2x já é melhorzinho`)
-                    } else if (randomNum < 80 && randomNum >= 70) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 1.3 }, { where: { user_id: user } })
-                        message.reply(`Tá melhorando, ganhou **${bet * 1.3} coins**, 1.3x maneiro`)
-                    } else if (randomNum < 88 && randomNum >= 80) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 1.4 }, { where: { user_id: user } })
-                        message.reply(`Aqui já fica legal, ganhou **${bet * 1.4} coins**, 1.4x insano`)
-                    } else if (randomNum < 94 && randomNum >= 88) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 1.5 }, { where: { user_id: user } })
-                        message.reply(`Forte demais, ganhou **${bet * 1.5} coins**, 1.5x pra vc`)
-                    } else if (randomNum < 97 && randomNum >= 94) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 2 }, { where: { user_id: user } })
-                        message.reply(`Dobrou sua aposta, ganhou **${bet * 2} coins**, 2x doados pra vc!`)
-                    } else if (randomNum < 98.7 && randomNum >= 97) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 4 }, { where: { user_id: user } })
-                        message.reply(`Aqui vc ja tá com a sorte em dia, ganhou **${bet * 4} coins**, 4x pra tu!`)
-                    } else if (randomNum < 99.5 && randomNum >= 98.7) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 7 }, { where: { user_id: user } })
-                        message.reply(`NÚMERO DA SORTE PRA ALGUÉM SORTUDO! Ganhou **${bet * 7} coins**, 7x a sua aposta!`)
-                    } else if (randomNum < 99.9 && randomNum >= 99.5) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 10 }, { where: { user_id: user } })
-                        message.reply(`VC É A PESSOA MAIS SORTUDA DO MUNDO!! Ganhou **${bet * 10} coins**, 10x o que vc apostou!!!`)
-                    } else if (randomNum <= 100 && randomNum >= 99.9) {
-                        const adding = await Usuario.update({ balance: row.balance + bet * 20 }, { where: { user_id: user } })
-                        message.reply(`VC SIMPLESMENTE ZEROU O TIGRINHO!!!! :tiger::tiger2: Gahou **${bet * 20} coins**, **20X** PRA VC AMIGÃO, PARABÉNS!!`)
+                        if (randomNum < 35) {
+                            message.reply('Vish ce sifudeu e perdeu tudo')
+                        } else if (randomNum < 55 && randomNum >= 35) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 1.1 }, { where: { user_id: user } })
+                            message.reply(`Nice mlk, ganhou **${parseInt(bet * 1.1)} coins**, 1.1x suave`)
+                        } else if (randomNum < 70 && randomNum >= 55) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 1.2 }, { where: { user_id: user } })
+                            message.reply(`Boa demais, ganhou **${parseInt(bet * 1.2)} coins**, 1.2x já é melhorzinho`)
+                        } else if (randomNum < 80 && randomNum >= 70) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 1.3 }, { where: { user_id: user } })
+                            message.reply(`Tá melhorando, ganhou **${parseInt(bet * 1.3)} coins**, 1.3x maneiro`)
+                        } else if (randomNum < 88 && randomNum >= 80) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 1.4 }, { where: { user_id: user } })
+                            message.reply(`Aqui já fica legal, ganhou **${parseInt(bet * 1.4)} coins**, 1.4x insano`)
+                        } else if (randomNum < 94 && randomNum >= 88) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 1.5 }, { where: { user_id: user } })
+                            message.reply(`Forte demais, ganhou **${parseInt(bet * 1.5)} coins**, 1.5x pra vc`)
+                        } else if (randomNum < 97 && randomNum >= 94) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 2 }, { where: { user_id: user } })
+                            message.reply(`Dobrou sua aposta, ganhou **${parseInt(bet * 2)} coins**, 2x doados pra vc!`)
+                        } else if (randomNum < 98.7 && randomNum >= 97) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 4 }, { where: { user_id: user } })
+                            message.reply(`Aqui vc ja tá com a sorte em dia, ganhou **${parseInt(bet * 4)} coins**, 4x pra tu!`)
+                        } else if (randomNum < 99.5 && randomNum >= 98.7) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 7 }, { where: { user_id: user } })
+                            message.reply(`NÚMERO DA SORTE PRA ALGUÉM SORTUDO! Ganhou **${parseInt(bet * 7)} coins**, 7x a sua aposta!`)
+                        } else if (randomNum < 99.9 && randomNum >= 99.5) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 10 }, { where: { user_id: user } })
+                            message.reply(`VC É A PESSOA MAIS SORTUDA DO MUNDO!! Ganhou **${parseInt(bet * 10)} coins**, 10x o que vc apostou!!!`)
+                        } else if (randomNum <= 100 && randomNum >= 99.9) {
+                            const adding = await Usuario.update({ balance: newRowBalance.balance + bet * 20 }, { where: { user_id: user } })
+                            message.reply(`VC SIMPLESMENTE ZEROU O TIGRINHO!!!! :tiger::tiger2: Gahou **${parseInt(bet * 20)} coins**, **20X** PRA VC AMIGÃO, PARABÉNS!!`)
+                        }
+                    } else {
+                        message.reply('Você não tem coins o suficiente para fazer a aposta!')
                     }
                 } else {
-                    message.reply('Você não tem coins o suficiente para fazer a aposta!')
+                    message.reply('Digite um valor válido para a aposta')
                 }
             } catch (error) {
                 console.error(error)
